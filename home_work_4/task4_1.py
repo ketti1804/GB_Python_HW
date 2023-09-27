a0483ed5326e289986ab6d2ee62c026f91efa899
Katerina_Zueva <ketti1804@mail.ru>
2023-09-27 16:56:30 +0300

    fixed syntax

--- home_work_4/task4_1.py
+++ home_work_4/task4_1.py
@@ -3,17 +3,16 @@
 Пользователь вводит 2 числа. n — кол-во элементов первого множества.
 m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств."""
 
-
 n = int(input("Введите количество элементов первого множества N: "))
 m = int(input("Введите количество элементов второго множества M: "))
-multitude1=[]
-multitude2=[]
+multitude1 = []
+multitude2 = []
 result = []
-for i in range(0,n):
-    multitude1.append(int(input(f"Введите {i+1}-й элемент первого множества: ")))
-for i in range(0,m):
-    multitude2.append(int(input(f"Введите {i+1}-й элемент второго множества: ")))
+for i in range(0, n):
+    multitude1.append(int(input(f"Введите {i + 1}-й элемент первого множества: ")))
+for i in range(0, m):
+    multitude2.append(int(input(f"Введите {i + 1}-й элемент второго множества: ")))
     if multitude2[i] in multitude1:
         result.append(multitude2[i])
 
-print(f"Итоговое множество: {set(result)}") 
+print(f"Итоговое множество: {set(result)}")
--- home_work_4/task4_2.py
+++ home_work_4/task4_2.py
@@ -10,22 +10,22 @@
 за один заход собирающий модуль, находясь перед некоторым кустом заданной во входном файле грядки."""
 
 n = int(input("Введите количество кустов N (не менее 3-х): "))
-while n <3:
+while n < 3:
     n = int(input("Вы ввели некорректное число. Введите количество кустов N (не менее 3-х): "))
     continue
 bush = []
 harvest = []
-for i in range (0,n):
-    bush.append(int(input(f"Введите количество ягод на {i+1}-м кусте: ")))
+for i in range(0, n):
+    bush.append(int(input(f"Введите количество ягод на {i + 1}-м кусте: ")))
 h_max = bush[1]
-for i in range (0,n):
+for i in range(0, n):
     if i == 0:
-        harvest.append(bush[0] + bush[1] + bush[n-1])
+        harvest.append(bush[0] + bush[1] + bush[n - 1])
     else:
-        if i != n-1:
-            harvest.append(bush[i-1] + bush[i] + bush[i+1])
+        if i != n - 1:
+            harvest.append(bush[i - 1] + bush[i] + bush[i + 1])
         else:
-            harvest.append(bush[n-2] + bush[n-1] + bush[0])
+            harvest.append(bush[n - 2] + bush[n - 1] + bush[0])
     if harvest[i] > h_max:
         h_max = harvest[i]
 
